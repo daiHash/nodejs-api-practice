41a118ad4ad24ff6d1606992da95311d49f5732b
daiHash <dhashi92@gmail.com>
2020-07-31 01:40:50 +0900

    Setup controller

---
+++ controllers/bootcamps.js
@@ -0,0 +1,24 @@
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  PUBLIC
+exports.getBootcamps = (req, res, next) => {}
+
+// @desc    Get a bootcamp
+// @route   GET /api/v1/bootcamps/:id
+// @access  PRIVATE
+exports.getBootcamp = (req, res, next) => {}
+
+// @desc    Create new bootcamp
+// @route   POST /api/v1/bootcamps
+// @access  PRIVATE
+exports.createBootcamp = (req, res, next) => {}
+
+// @desc    Update bootcamp
+// @route   PUT /api/v1/bootcamps/:id
+// @access  PRIVATE
+exports.updateBootcamp = (req, res, next) => {}
+
+// @desc    Delete bootcamp
+// @route   DELETE /api/v1/bootcamps/:id
+// @access  PRIVATE
+exports.deleteBootcamps = (req, res, next) => {}
