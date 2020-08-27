4c742bfeabb6e334e467438fafc47aad241a157e
daiHash <dhashi92@gmail.com>
2020-08-28 01:03:21 +0900

    Add controller for bootcamnps within a radius

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse')
-const Bootcamp = require('../models/Bootcamp')
 const asyncHandler = require('../middleware/asyncHandler')
+const geocoder = require('../utils/geocoder')
+const Bootcamp = require('../models/Bootcamp')
 
 // @desc    Get all bootcamps
 // @route   GET /api/v1/bootcamps
@@ -81,5 +82,32 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   res.status(200).json({
     success: true,
     data: {},
+  })
+})
+
+// @desc    Get bootcamps within a radius
+// @route   DELETE /api/v1/bootcamps/radius/:zipcode/:distance
+// @access  PRIVATE
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params
+
+  // Get lat / lng from geocoder
+  const location = await geocoder.geocode(zipcode)
+  const lat = location[0].latitude
+  const lng = location[0].longitude
+
+  // Calculate radius using radians
+  // Divide distance by radius of Earth
+  // Earth Radius = 3950 miles / KM 6378km
+  const radius = distance / 6378
+
+  const bootcamps = await Bootcamp.find({
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  })
+
+  res.status(200).json({
+    success: true,
+    size: bootcamps.length,
+    data: bootcamps,
   })
 })
