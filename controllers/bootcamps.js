dba0b26e27f97a671f9185f9aa6f92518cb22b44
daiHash <dhashi92@gmail.com>
2020-09-24 00:35:48 +0900

    Add filter for quantity

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,11 +7,21 @@ const Bootcamp = require('../models/Bootcamp')
 // @route   GET /api/v1/bootcamps
 // @access  PUBLIC
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find()
+  let query
+
+  let queryString = JSON.stringify(req.query)
+
+  queryString = queryString.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  )
+
+  query = Bootcamp.find(JSON.parse(queryString))
+  const bootcamps = await query
 
   res.status(200).json({
     success: true,
-    sizes: bootcamps.length,
+    size: bootcamps.length,
     data: bootcamps,
   })
 })
