3130ce16d8a12543c24cab8ddc994048882daf69
daiHash <dhashi92@gmail.com>
2020-10-14 00:24:32 +0900

    Fix typo

--- models/Course.js
+++ models/Course.js
@@ -5,7 +5,7 @@ const CourseSchema = new mongoose.Schema({
   description: { type: String, required: [true, 'Please add a description'] },
   weeks: { type: String, required: [true, 'Please add number of weeks'] },
   tuition: { type: Number, required: [true, 'Please add a tuition cost'] },
-  minimumSkill: { type: String, required: [true, 'Please add a minimum skill'], enum: ['begginer', 'intermediate', 'advanced'] },
+  minimumSkill: { type: String, required: [true, 'Please add a minimum skill'], enum: ['beginner', 'intermediate', 'advanced'] },
   scholarshipAvailable: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
   bootcamp: { type: mongoose.Schema.ObjectId, ref: 'Bootcamp', required: true }
