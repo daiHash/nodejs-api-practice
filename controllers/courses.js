2cd98fb02619110fa5621066dc16784497cead5e
daiHash <dhashi92@gmail.com>
2020-10-14 00:57:29 +0900

    Add courses routes

---
+++ controllers/courses.js
@@ -0,0 +1,24 @@
+const ErrorResponse = require('../utils/errorResponse')
+const asyncHandler = require('../middleware/asyncHandler')
+const Course = require('../models/Course')
+
+// @desc    Get all courses
+// @route   GET /api/v1/courses
+// @route   GET /api/v1/bootcamps/:bootcampId/courses
+// @access  PUBLIC
+exports.getCourses = asyncHandler(async (req, res, next) => {
+  let query
+  if (req.params.bootcampId) {
+    query = Course.find({ bootcamp: req.params.bootcampId })
+  } else {
+    query = Course.find()
+  }
+
+  const courses = await query
+
+  res.status(200).json({
+    success: true,
+    size: courses.length,
+    data: courses,
+  })
+})
