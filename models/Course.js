55ab9bdcb48f11bda8213c8b2145a34338610e6c
daiHash <dhashi92@gmail.com>
2020-10-16 01:14:03 +0900

    Add static to get average tuition of courses of a bootcamp

--- models/Course.js
+++ models/Course.js
@@ -11,5 +11,33 @@ const CourseSchema = new mongoose.Schema({
   bootcamp: { type: mongoose.Schema.ObjectId, ref: 'Bootcamp', required: true }
 })
 
+// * Static method to get average course tuition
+CourseSchema.statics.getAverageCost = async function(bootcampId) {
+  console.log('Calculating avg cost...'.blue)
+
+  const obj = await this.aggregate([
+    { $match: { bootcamp: bootcampId } },
+    { $group: { _id: '$bootcamp', averageCost: { $avg: '$tuition' } } }
+  ])
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10
+    })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+// * Call getAverageCost after save
+CourseSchema.post('save', function() {
+  this.constructor.getAverageCost(this.bootcamp)
+})
+
+// * Call getAverageCost after remove
+CourseSchema.pre('remove', function() {
+  this.constructor.getAverageCost(this.bootcamp)
+})
+
 module.exports = mongoose.model('Course', CourseSchema)
 
