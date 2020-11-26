cdca8b71e34b246a7deefa8b23b979254738943d
daiHash <dhashi92@gmail.com>
2020-11-27 00:05:57 +0900

    Add user model and routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ const User = require('../models/User')
 exports.register = asyncHandler(async (req, res, next) => {
   const { name, email, password, role } = req.body
 
-  //* Craete User
+  //* Create User
   const user = await User.create({ name, email, password, role })
   res.status(200).json({ success: true })
 })
